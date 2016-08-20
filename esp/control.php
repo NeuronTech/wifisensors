3ffd92402f1e80fd89a6885c24b83043ab58ae38
Mike <mikeoindub@gmail.com>
2016-08-20 11:54:31 +0100

    Converting to new code

---
+++ esp/control.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+*
+* @package ESP8266 Web Server
+* @version $Id$
+* @author  Michael O'Toole - aka michaelo
+* @begin   Saturday, Jan 22, 2015
+* @copyright (c) 2015 phpbbireland
+* @home    http://www.phpbbireland.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+define('IN_CODE', true);
+
+$root_path = (defined('ROOT_PATH')) ? ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($root_path . 'common.' . $phpEx);
+include($root_path . 'includes/functions_main.' . $phpEx);
+include($root_path . 'includes/functions.' . $phpEx);
+
+if (!$auth)
+{
+	header('Location: login.php');
+	exit;
+}
+
+
+include('header.html');
+include('left_blocks.html');
+include('section_control_form.html');
+include('footer.html');
